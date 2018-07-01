44e62244ba03ff64b49d82f7d947b1ca8d92d938
achillesion <achillesion@gmail.com>
2018-07-01 20:27:03 +0500

    Imported HTTP module in imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { PostsComponent } from './posts/posts.component';
 import { UsersComponent } from './users/users.component';
 import { DetailsComponent } from './details/details.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     PostsComponent,
     UsersComponent,
-    DetailsComponent
+    DetailsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
